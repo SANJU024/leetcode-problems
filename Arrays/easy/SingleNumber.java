d38e9061aafd9a60e499c832bbbbfc32b9c52175
sanjay <sanjaysanju35385@gmail.com>
2025-04-16 02:35:12 +0530

    some changes

---
+++ Arrays/easy/SingleNumber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        // Map<Integer,Integer> hm = new HashMap<>();
+        // for(int num: nums){
+        //     hm.put(num,hm.getOrDefault(num,0)+1);
+        // }
+
+        // for(Map.Entry<Integer,Integer> entry: hm.entrySet()){//{2=2,1=1}
+        //     if(entry.getValue()==1){
+        //         return entry.getKey();
+        //     }
+        // }
+        // return -1;
+
+        int result=0;
+        for(int num:nums){
+            result^=num;
+        }
+        return result;
+    }
+    
+}
